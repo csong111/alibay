c961653447f835ace6b46354d348bdeeda4e4862
aishad <aishadiallo@gmail.com>
2018-05-07 19:34:17 -0400

    'updates'

---
+++ aisha.js
@@ -0,0 +1,46 @@
+const assert = require('assert');
+
+let itemsBought = {
+    userID : {
+        itemID : 123,
+        itemID : 123,
+        itemID : 472
+    }
+} // map that keeps track of all the items a user has bought
+let itemsSold = {
+    userID : {
+        itemID : 123,
+        itemID : 123,
+        itemID : 472
+    } 
+} // map that keeps track of all the items a user has sold
+let listings = {
+    itemID : {
+        userID: 222,
+        price: 40.99,
+        description: "blue sweater from the 1980s",
+        itemName: "vintage 80s sweater",
+        image: "url"
+    }
+} // map that keeps track of all the items being sold on the marketplace
+
+
+let user = {
+    userID: {
+        email: "email@email.com",
+        firstName: "Mary",
+        lastName: "doe",
+        password: "password"
+    }
+}
+
+//function to create a user
+function signUp (){
+    
+}
+
+//function to 
+function login (){
+
+}
+
