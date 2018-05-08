f81413b5cb97500c66ad016eafd523c2cec5e074
aishad <aishadiallo@gmail.com>
2018-05-08 12:02:13 -0400

    'aisha changes'

--- aisha.js
+++ aisha.js
@@ -1,7 +1,11 @@
-const assert = require('assert');
+const alibay = require('./alibay')
+const express = require('express')
+const app = express()
+const bodyParser = require('body-parser')
+const fs = require("fs")
 
-let user = {
-    userID: {
+let users = {
+    4145241: {
         email: "email@email.com",
         firstName: "Mary",
         lastName: "doe",
@@ -9,20 +13,46 @@ let user = {
     }
 }
 
-//function to create a user
-function signUp (email, firstName, lastName, password){
-
+let sessionInfo={}
+    
+try {
+    users = JSON.parse(fs.readFileSync('../users.json').toString())
+} catch (err) {
+}
+function genUID() {
+    return Math.floor(Math.random() * 100000000)
 }
 
-
-//function to 
-function login (email, password){
-    let email = email
-    let password = password
-    if (user[email]=== password){
-        
+function signUp (email, password, firstName, lastName){
+    let userID = genUID
+    while (users[userID]){
+        let userID = genUID
     }
 
+    Object.keys(users.forEach((user,ind) =>{
+        if (users[user].email === email){
+            return {success:false}
+        }
+    }))
+    users[userID] = {email, password, firstName, lastName}
+    fs.writeFileSync('../users.json', JSON.stringify(users))
+    return {success:true}
+}    
+
+function login(email, password){
+// go through all the users, find the userID with the email....
+
+
+
+if(users[userID].email===email ){
+    let sessionID = Math.floor(Math.random() * 10000000);
+    sessionInfo[sessionID] = email;
+
+}    
 
 }
 
+module.exports = {
+    signUp,
+    login
+}
